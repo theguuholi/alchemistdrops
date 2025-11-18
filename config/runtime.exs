0cd85330bf41dd89eab1876b11354551b75e34df
Gustavo <gustavo.oliveira@stord.com>
2025-11-18 07:04:41 -0300

    setup

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
   config :alchemistdrops, Alchemistdrops.Repo,
     # ssl: true,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
     socket_options: maybe_ipv6
