f49d735d072868fcd3ecba17998b6c07da050930
Gustavo <gustavo.oliveira@stord.com>
2025-11-19 09:00:36 -0300

    update check origin

--- config/runtime.exs
+++ config/runtime.exs
@@ -65,7 +65,12 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: [
+      "https://#{host}",
+      "https://www.alchemistdrops.com",
+      "https://alchemistdrops.com"
+    ]
 
   # ## SSL Support
   #
