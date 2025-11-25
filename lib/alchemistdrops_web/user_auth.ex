518b2a452549ad5204cac18a2074f53d8f583e60
Gustavo <gustavo.oliveira@stord.com>
2025-11-25 09:10:15 -0300

    fix

--- lib/alchemistdrops_web/user_auth.ex
+++ lib/alchemistdrops_web/user_auth.ex
@@ -248,7 +248,8 @@ defmodule AlchemistdropsWeb.UserAuth do
   def on_mount(:require_admin_user, _params, session, socket) do
     socket = mount_current_scope(socket, session)
 
-    if socket.assigns.current_scope.user.role == :admin do
+    if socket.assigns.current_scope && socket.assigns.current_scope.user &&
+         socket.assigns.current_scope.user.role == :admin do
       {:cont, socket}
     else
       {:halt, socket}
